b0da5c3bad75bacdc472349d06e21943f372d585
James Larivee <mr_15th_entertainer@yahoo.com>
2019-10-07 16:55:10 -0600

    Added fragments for different hands.

--- app/src/main/java/edu/cnm/deepdive/blackjack/model/dao/HandDao.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/model/dao/HandDao.java
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Update;
 import edu.cnm.deepdive.blackjack.model.entity.Hand;
+import edu.cnm.deepdive.blackjack.model.pojo.HandWithCards;
 import java.util.List;
 
 @Dao
@@ -14,14 +16,17 @@ public interface HandDao {
   @Insert
   long[] insert(Hand... hands);
 
-  @Query("SELECT * FROM Hand WHERE round_id =:roundId ORDER BY dealer DESC")
+  @Query("SELECT * FROM Hand WHERE round_id = :roundId ORDER BY dealer DESC")
   LiveData<List<Hand>> getAllByRoundId(long roundId);
 
-  @Query("SELECT * FROM Hand WHERE hand_id =:handId")
+  @Query("SELECT * FROM Hand WHERE hand_id = :handId")
   LiveData<Hand> getByHandId(long handId);
+
+  @Transaction
+  @Query("SELECT * FROM Hand WHERE hand_id = :handId")
+  LiveData<HandWithCards> getHandWithCards(long handId);
 
   @Update
   int update(Hand hand);
-
 
 }
