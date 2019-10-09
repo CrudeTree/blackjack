59b1bd35fc90f64766fdc1934e52de270fe7a3da
James Larivee <mr_15th_entertainer@yahoo.com>
2019-10-09 10:02:23 -0600

    Implemented Hit Me Button

--- app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/MainActivity.java
@@ -1,16 +1,12 @@
 package edu.cnm.deepdive.blackjack.controller;
 
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.blackjack.R;
-import edu.cnm.deepdive.blackjack.model.entity.Card;
 import edu.cnm.deepdive.blackjack.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,17 +20,6 @@ public class MainActivity extends AppCompatActivity {
     });
     Button addDeck = findViewById(R.id.start_round);
     addDeck.setOnClickListener((view) -> viewModel.startRound());
-    setupFragments();
-  }
-
-  private void setupFragments() {
-    Fragment dealerFragment = new DealerHandFragment();
-    Fragment playerFragment = new PlayerHandFragment();
-    FragmentManager manager = getSupportFragmentManager();
-    manager.beginTransaction()
-        .replace(R.id.dealer_hand, dealerFragment)
-        .replace(R.id.player_hand, playerFragment)
-        .commit();
   }
 
 }
--- app/src/main/java/edu/cnm/deepdive/blackjack/controller/PlayerHandFragment.java
+++ app/src/main/java/edu/cnm/deepdive/blackjack/controller/PlayerHandFragment.java
@@ -1,22 +1,36 @@
 package edu.cnm.deepdive.blackjack.controller;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProviders;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import androidx.lifecycle.LiveData;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import edu.cnm.deepdive.blackjack.R;
+import edu.cnm.deepdive.blackjack.model.pojo.HandWithCards;
 import edu.cnm.deepdive.blackjack.viewmodel.MainViewModel;
 
 public class PlayerHandFragment extends HandFragment {
 
   @Override
-  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    super.onViewCreated(view, savedInstanceState);
-    MainViewModel viewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);
-    viewModel.getPlayerHand().observe(this, (hand) -> {
-      // Set Content of List View Adapter.
-    });
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = super.onCreateView(inflater, container, savedInstanceState);
+    FloatingActionButton hitMe = view.findViewById(R.id.hit_me);
+    hitMe.setOnClickListener((v) -> getViewModel().hitPlayer());
 
+    return view;
+  }
+
+  @Override
+  public LiveData<HandWithCards> handToObserve(MainViewModel viewModel) {
+    return viewModel.getPlayerHand();
+  }
+
+  @Override
+  public int getLayout() {
+    return R.layout.fragment_player_hand;
   }
 
 }
